91c7616a8f6fb8c7c176331f28e79935bc9459d3
Tasnim562 <selmitasnim4@gmail.com>
2025-12-27 23:00:32 +0100

    new db elemnts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -15,9 +15,18 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Media files
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
@@ -25,3 +29,9 @@ urlpatterns = [
     path('gastronomie/', include('gastronomie.urls')),
     path('events/', include('events.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
